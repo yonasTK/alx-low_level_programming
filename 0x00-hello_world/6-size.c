de5d46abf9b1df3172029b6aeee66662c488629e
yonas <yonaskene.yk@gmail.com>
2022-06-29 01:29:25 -0700

    betty error corrction

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,17 +8,17 @@
 
 int main(void)
 {
-  char acharacter;
-  int ainteger;
-  long along;
-  long long dlong;
-  float flot;
+char acharacter;
+int ainteger;
+long along;
+long long dlong;
+float flot;
 
-  printf("Size of a char: %lu byte(s)\n", sizeof(acharacter));
-  printf("Size of an int: %lu byte(s)\n", sizeof(ainteger));
-  printf("Size of a long int: %lu byte(s)\n", sizeof(along));
-  printf("Size of a long long int: %lu byte(s)\n", sizeof(dlong));
-  printf("Size of a float: %lu byte(s)\n", sizeof(flot));
+printf("Size of a char: %lu byte(s)\n", sizeof(acharacter));
+printf("Size of an int: %lu byte(s)\n", sizeof(ainteger));
+printf("Size of a long int: %lu byte(s)\n", sizeof(along));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(dlong));
+printf("Size of a float: %lu byte(s)\n", sizeof(flot));
 
-  return (0);
+return (0);
 }
