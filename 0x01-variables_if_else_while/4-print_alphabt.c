aee0cc137e3b15e48b7af9ba43c73805cf6b21bf
yonasTK <48440474+yonasTK@users.noreply.github.com>
2022-07-01 12:11:02 +0300

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,14 +7,13 @@
  */
 int main(void)
 {
-  char letter;
+char letter;
 
-  for (letter = 'a'; letter <= 'z'; letter++)
-    {
-      if (letter != 'e' && letter != 'q')
-	putchar(letter);
-    }
-  putchar('\n');
-
-  return (0);
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+if (letter != 'e' && letter != 'q')
+putchar(letter);
+}
+putchar('\n');
+return (0);
 }
