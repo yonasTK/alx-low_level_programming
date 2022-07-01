cde3e53745e5448f6726250211aef681127537df
yonasTK <48440474+yonasTK@users.noreply.github.com>
2022-07-01 12:17:18 +0300

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,24 +8,23 @@
 
 int main(void)
 {
-  int ones;
-  int tens;
+int ones;
+int tens;
 
-  for (tens = '0'; tens <= '9'; tens++) /*increment tens*/
-    {
-      for (ones = (tens + 1); ones <= '9'; ones++) /*one's ten+1*/
-	{
-	  putchar(tens);
-	  putchar(ones);
+for (tens = '0'; tens <= '9'; tens++) /*increment tens*/
+{
+for (ones = (tens + 1); ones <= '9'; ones++) /*one's ten+1*/
+{
+putchar(tens);
+putchar(ones);
 
-	  if (tens != '8' || ones != '9') /*print commas*/
-	    {
-	      putchar(',');
-	      putchar(' ');
-	    }
-	}
-    }
-  putchar('\n');
-
-  return (0);
+if (tens != '8' || ones != '9') /*print commas*/
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
 }
