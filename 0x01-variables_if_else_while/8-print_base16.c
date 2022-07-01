fb1c163f83d7bd2872213c501752f2ee611ccf44
yonasTK <48440474+yonasTK@users.noreply.github.com>
2022-07-01 12:14:00 +0300

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,16 +7,16 @@
  */
 int main(void)
 {
-  char digit;
+char digit;
 
-  for (digit = '0'; digit <= '9'; digit++)
-    {
-      putchar(digit);
-    }
-  for (digit = 'a'; digit <= 'f'; digit++)
-    {
-    putchar(digit);
-    }
+for (digit = '0'; digit <= '9'; digit++)
+{
+putchar(digit);
+}
+for (digit = 'a'; digit <= 'f'; digit++)
+{
+putchar(digit);
+}
 
 putchar('\n');
 
