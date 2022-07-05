d88944b2a3138ffc89e792d8c87e083ae49eb2b1
yonasTK <48440474+yonasTK@users.noreply.github.com>
2022-07-05 16:24:20 +0300

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
-*  * add - add two integers
-*   * @a: two integer arguments
-*    * @b: two integer arguments
-*     * Return: sum
-*      */
+* add - add two integers
+* @a: two integer arguments
+* @b: two integer arguments
+* Return: sum
+* /
 
 int add(int a, int b)
 {
