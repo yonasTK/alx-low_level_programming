39098424031dba815d57c5843572480d87e2f7a8
yonasTK <48440474+yonasTK@users.noreply.github.com>
2022-07-05 16:36:21 +0300

    Update 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
 /**
-*  main - Prints natural numbers below 1024 that are
-*  multiplies of 3 or 5 
+*  main - Prints natural numbers below 1024 that are multiplies of 3 or 5 
 *  Return: Always 0.
 */
 int main(void)
