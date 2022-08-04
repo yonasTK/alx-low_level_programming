bd6e3d0f2725c5a5c0449055ecb3f91e60fabf6d
yonasTK <48440474+yonasTK@users.noreply.github.com>
2022-08-04 12:05:40 +0300

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,19 +1,23 @@
-#ifndef VARFUNC
-#define VARFUNC
+#ifndef VARIADIC_FUNCTIONS
+#define VARIADIC_FUNCTIONS
+
 #include <stdarg.h>
+
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
 /**
- * struct typ - Struct operators
- *
- * @c: Char
- * @tp: The function associated
- */
-typedef struct typ
+*struct dt - struct
+*@letter: letter signifying data type
+*@func: function ptr
+*/
+typedef struct dt
 {
-	char *c;
-	void (*tp)(va_list var);
-} typ_t;
+char letter;
+void (*func)(va_list);
+} datatype;
+
 #endif
