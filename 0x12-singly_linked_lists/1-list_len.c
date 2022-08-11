39df3bf93a5b5059b27088c67ee6625dcfb99827
yonasTK <48440474+yonasTK@users.noreply.github.com>
2022-08-11 11:03:50 +0300

    Update 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,14 +3,14 @@
 #include <stdlib.h>
 /**
  * list_len - finds the number of nodes in a list
- * @h: singly linked list
+ * @sl: singly linked list
  * Return: number of elements in a linked list
  */
-size_t list_len(const list_t *h)
+size_t list_len(const list_t *sl)
 {
 	int i;
 
-	for (i = 0; h; i++)
-		h = h->next;
+	for (i = 0; sl; i++)
+		sl = sl->next;
 	return (i);
 }
