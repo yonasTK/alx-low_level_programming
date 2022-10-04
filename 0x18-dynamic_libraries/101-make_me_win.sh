94446b42fe02e2e10cde023858d7b99b8a029339
Yonas Kenenisa <48440474+yonasTK@users.noreply.github.com>
2022-10-04 15:23:37 +0300

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cp ../sh2.sh .
-. ./sh2.sh
+wget -P /tmp https://github.com/arthurdamm/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/libwinner.so
+export LD_PRELOAD=/tmp/libwinner.so
